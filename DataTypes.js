ba929c5184b07ce6542095a9ffa018f6290b1be8
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 15:57:10 +0530

    DataTypes in JS

---
+++ DataTypes.js
@@ -0,0 +1,49 @@
+//Data Types In JavaScript
+// 1. Premetives : undefined,null,number,string,boolean,symbol
+// 2. Refrence : Arrays,Objects
+
+//1.Numbers
+
+var num1 = 45
+var num2 = 98.99
+
+console.log(num1)
+console.log(num2)
+
+//2.String
+
+var str1 = "This is Yogesh"
+var str2 = 'This is Yogesh'
+
+console.log(str1)
+console.log(str2)
+
+//3.Object
+
+var marks = {
+    pavan : 45,
+    yogesh : 50
+}
+
+console.log(marks)
+
+//4.Boolean
+
+var a = true
+var b = false
+
+console.log(a)
+console.log(b)
+
+//4.Undefined
+
+// var und = undefined;
+var und;
+console.log(und);
+
+var n =null;
+console.log(n);
+
+// Arrays
+
+var arr = [1,2,3,4,"Yogesh"]
