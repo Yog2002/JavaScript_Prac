9f949ce5147c7841c14b821a2484a26c9a72120a
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 16:32:06 +0530

    Functions in JS

---
+++ Functions.js
@@ -0,0 +1,10 @@
+//Functions In JavaScript
+// DRY = Do Not Repeat Yourself
+
+function avg(a,b){
+    return (a+b)/2;
+}
+
+c1 = avg(4,6);
+c2 = avg(14,16);
+console.log(c1,c2);
--- index.js
+++ index.js
@@ -103,6 +103,17 @@ console.log(false || false)
 console.log(!false)
 console.log(!true)
 
+//Functions In JavaScript
+// DRY = Do Not Repeat Yourself
+
+function avg(a,b){
+    return (a+b)/2;
+}
+
+c1 = avg(4,6);
+c2 = avg(14,16);
+console.log(c1,c2);
+
 
 
 
