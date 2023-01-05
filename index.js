dfff2e3cfc533f62846ecd3ac8a06e701e03bb81
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 17:24:20 +0530

    Loops in JS

--- index.js
+++ index.js
@@ -149,6 +149,35 @@ else{
 }
 console.log("End of ladder");
 
+//Loops in JS
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr);
+for(var i=0;i<arr.length;i++){
+    if(i==2){
+        // break;
+        continue;
+    }
+    console.log(arr[i])
+}
+
+arr.forEach(function(element){
+    console.log(element);
+})
+
+
+while(j<arr.length){
+    console.log(arr[j]);
+    j ++;
+ }
+
+do{
+    console.log(arr[j]);
+    j++;
+} while (j < arr.length);
+
+
+
 
 
 
