03e3f878865721803488e8df5caaec521be1c06a
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 16:19:54 +0530

    Operators  in Js

---
+++ operator.js
@@ -0,0 +1,48 @@
+// Operators In JavaScript
+//1.Arithmetic Operators
+
+var a = 100;
+var b = 10;
+console.log('The value of a+b is', a+b);
+console.log('The value of a+b is', a-b);
+console.log('The value of a+b is', a*b);
+console.log('The value of a+b is', a/b);
+
+//2.Assignment Operaotors
+
+var c = b
+c =+ b; // c = c + b
+c -= b;
+c *= b;
+c /= b;
+
+//3.Comparison Operators
+
+var x =35
+var y = 30
+console.log(x == y)
+console.log(x >= y)
+console.log(x <= y)
+console.log(x > y)
+console.log(x < y)
+
+//4.Logical Operators
+
+//Logical AND (&&)
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+//Logical OR (||)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+//Logical NOT (!)
+
+console.log(!false)
+console.log(!true)
