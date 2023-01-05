853298a19a856e7b21ab365f33d56c7088472cdb
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 16:59:58 +0530

    Conditional Statement in JS

---
+++ conditions.js
@@ -0,0 +1,34 @@
+// Conditionals in JavaScript
+
+var age = 41;
+// Single if statement
+if(age > 18){
+    console.log('You can drink rasna water');
+}
+// if - else statement
+if(age > 18){
+    console.log('You can drink rasna water');
+}
+else{
+    console.log('You cannot drink rasna water');
+}
+
+// if-else Ladder
+
+age = 25;
+if(age > 32){
+    console.log("You are not a kid");
+}
+else if(age >26){
+    console.log("Bachhe nahi rahe");
+}
+else if(age >22){
+    console.log("Yes Bachhe nahi rahe");
+}
+else if(age >18){
+    console.log("18 Bachhe nahi rahe");
+}
+else{
+    console.log("Bachhe rahe");
+}
+console.log("End of ladder");
