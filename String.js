5be750d6c6e2a635b5a01800b3ef76e73c83a744
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-05 17:58:10 +0530

    Strings in JS

---
+++ String.js
@@ -0,0 +1,11 @@
+// String Methods in JavaScript
+
+let myLovelyString = "Harry is a good boy good good Harry";
+console.log(myLovelyString.length)
+console.log(myLovelyString.indexOf("good"))
+console.log(myLovelyString.lastIndexOf("good"))
+
+// console.log(myLovelyString.slice(1,4))
+d = myLovelyString.replace("Harry", "Rohan");
+// d = d.replace("good", "bad");
+// console.log(d, myLovelyString)
