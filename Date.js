176af53a03ccc07cff9eb14be91ccb6504914be2
Yogesh Chaudhari <yochaudhari2002@gmail.com>
2023-01-06 11:44:17 +0530

    Date in JS

---
+++ Date.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+ 
